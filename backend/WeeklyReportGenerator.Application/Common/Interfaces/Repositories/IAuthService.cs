67594a07b3aa737e0f048c794faf9a8e410b9be5
alasaneeth <alasaneeth22@gmail.com>
2026-09-03 17:58:25 +0530

    feat(application): add repository, unit of work, and auth service interfaces

---
+++ backend/WeeklyReportGenerator.Application/Common/Interfaces/Repositories/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using WeeklyReportGenerator.Application.DTOs.Auth;
+
+namespace WeeklyReportGenerator.Application.Common.Interfaces;
+
+public interface IAuthService
+{
+    Task<bool> RegisterAsync(RegisterRequestDto request);
+    Task<LoginResponseDto> LoginAsync(LoginRequestDto request);
+    Task<LoginResponseDto> RefreshTokenAsync(string refreshToken);
+    Task RevokeTokenAsync(string refreshToken);
+}
---
+++ backend/WeeklyReportGenerator.Application/Common/Interfaces/Repositories/IJwtService.cs
@@ -0,0 +1,11 @@
+﻿using WeeklyReportGenerator.Domain.Entities;
+
+namespace WeeklyReportGenerator.Application.Common.Interfaces;
+
+public interface IJwtService
+{
+    string GenerateAccessToken(User user);
+    string GenerateRefreshToken();
+    DateTime GetAccessTokenExpiry();
+    DateTime GetRefreshTokenExpiry();
+}
