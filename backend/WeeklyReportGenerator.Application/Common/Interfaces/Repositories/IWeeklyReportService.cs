51b14b9c2fc7ae0facae34abee536c276558da9f
alasaneeth <alasaneeth22@gmail.com>
2026-09-05 05:25:37 +0530

    feat(application): add IWeeklyReportRepository and IWeeklyReportService interfaces

---
+++ backend/WeeklyReportGenerator.Application/Common/Interfaces/Repositories/IWeeklyReportService.cs
@@ -0,0 +1,12 @@
+﻿using WeeklyReportGenerator.Application.DTOs.Reports;
+
+namespace WeeklyReportGenerator.Application.Common.Interfaces;
+
+public interface IWeeklyReportService
+{
+    Task<WeeklyReportResponseDto> CreateDraftAsync(Guid userId, SaveWeeklyReportDto dto);
+    Task<WeeklyReportResponseDto> UpdateAsync(Guid userId, Guid reportId, SaveWeeklyReportDto dto);
+    Task<WeeklyReportResponseDto> SubmitAsync(Guid userId, Guid reportId);
+    Task<WeeklyReportResponseDto> GetByIdAsync(Guid userId, Guid reportId, bool isManager);
+    Task<IEnumerable<WeeklyReportSummaryDto>> GetMyHistoryAsync(Guid userId);
+}
